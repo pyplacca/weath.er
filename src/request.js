91abe1775564c6acddca3b9044901ad8ef3c9270
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-07 02:12:45 +0000

    move api key to env file and reformat file

--- src/request.js
+++ src/request.js
@@ -1,44 +1,58 @@
-const API_URL = 'http://api.airvisual.com/v2/'
-const KEY = 'd4257be2-a1b9-4243-887a-abca5ec13174'
-
 function Fetch (url) {
 	// const request_options = {
 	// 	method: 'GET',
 	// 	redirect: 'follow',
 	// 	credentials: 'same-origin',
 	// }
-	console.log(url)
 	return fetch(url)
 		.then(response => response.json())
-	  	.catch(error => {
-	  		alert('Couldn\'t load data. Please check your internet connection and try again')
-	  		console.log('error', error)
-	  	});
+  	.catch(error => {
+  		alert('Couldn\'t load data. Please check your internet connection and try again')
+  		console.log('error', error)
+  	});
 }
 
 const APIrequest = {
 
 	getWeather (country, state, city) {
 		return Fetch(
-			`${API_URL}city?city=${city}&state=${state}&country=${country}&key=${KEY}`
+			`
+			${process.env.REACT_APP_API_URL}city?
+			city=${city}&
+			state=${state}&
+			country=${country}&
+			key=${process.env.REACT_APP_API_KEY}
+			`
 		)
 	},
 
 	getCountries () {
 		return Fetch(
-			`${API_URL}countries?key=${KEY}`,
+			`
+			${process.env.REACT_APP_API_URL}countries?
+			key=${process.env.REACT_APP_API_KEY}
+			`,
 		)
 	},
 
 	getStates (country) {
 		return Fetch(
-			`${API_URL}states?country=${country}&key=${KEY}`
+			`
+			${process.env.REACT_APP_API_URL}states?
+			country=${country}&
+			key=${process.env.REACT_APP_API_KEY}
+			`
 		)
 	},
 
 	getCities (country, state) {
 		return Fetch(
-			`${API_URL}cities?state=${state}&country=${country}&key=${KEY}`,
+			`
+			${process.env.REACT_APP_API_URL}cities?
+			state=${state}&
+			country=${country}&
+			key=${process.env.REACT_APP_API_KEY}
+			`,
 		)
 	},
 }
